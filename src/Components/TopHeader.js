c65d583fd3472c251ef724713e23b0bbc50168b4
Peddigari <shruthipeddigari445@gmail.com>
2023-08-18 19:21:13 +0530

    changed

--- src/Components/TopHeader.js
+++ src/Components/TopHeader.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import Hamburger from "../Assets/bars-solid.svg" 
+import Hamburger from "../Assets/bars-solid.svg"
 // import Home from './Home';
 const TopHeader = (props) => {
     const navigate = useNavigate();
 
-    
-    const ClickHandle=()=>{
+
+    const ClickHandle = () => {
         props.setShowNav(!props.showNav)
     }
 
     return (
         <div className="topText">
-            <div className="title" onClick={()=>{navigate("/")} }>
-            <div className="the">The</div>
-            <div className='siren'>Siren</div>
+            <div className="title" onClick={() => { navigate("/") }}>
+                <div className="the">The</div>
+                <div className='siren'>Siren</div>
             </div>
             <div onClick={ClickHandle} className="hamburger">
                 <img src={Hamburger} alt="not found"></img>
             </div>
         </div>
-       
+
     )
 }
 
